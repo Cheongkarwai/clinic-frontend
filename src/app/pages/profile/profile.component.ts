348663beb1e9586e157307bb6c8cbd595b4c39c3
Cheong Kar Wai <cheongkarwai5@gmail.com>
2023-07-20 22:11:03 +0800

    refactor-separating into multiple components

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,49 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { faUser,faKey,faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { Observable, map, tap } from 'rxjs';
+import { UserProfile } from 'src/app/core/user/user-profile.model';
+import { UserService } from 'src/app/core/user/user.service';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { Router } from '@angular/router';
+import { UserCredentials } from 'src/app/core/user/user-credentials.model';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
 
   //fontawesome icon
   faUser = faUser;
   faKey = faKey;
   faArrowRightFromBracket = faArrowRightFromBracket;
 
-  isEditing:boolean = false;
-  isSavedProfile:boolean = false;
-
   //current active tab
-  currentActiveTab:string = 'Edit-profile';
+  activeTabIndex:number = 0;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
-  ngOnInit(): void {
+  logout(event:any){
+    sessionStorage.clear();
+    this.router.navigateByUrl('/login');
   }
-
-  toggleEditProfile(event:any){
-    this.isEditing = !this.isEditing;
-  }
-
-  editProfile(event:any){
-    this.isEditing = !this.isEditing;
-    this.isSavedProfile = true;
-
-    setTimeout(()=>{
-      this.isSavedProfile = false;
-    },3000);
-  }
-
-  changeUserPassword(event:any){}
-
-  changeUserProfile(event:any){}
-
-  logout(event:any){}
   
-  switchProfileActiveTab(activeTab:string){
-    this.currentActiveTab = activeTab;
+  switchProfileActiveTab(index:number){
+    this.activeTabIndex = index;
   }
+
 }
